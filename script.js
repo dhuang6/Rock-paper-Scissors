d13e205c7ac6bf6803f0563451c157e65cea1726
Daniel Huang <dhuang684@gmail.com>
2018-11-26 11:00:17 -0500

    updated script file
    stored the 5 rounds into an array to be displayed at the end.

--- script.js
+++ script.js
@@ -1,55 +1,67 @@
-
-var results = [];
-//generate a random number between 0 and 1 using Math.random(); split it so each quarter is an answer
-var computerplayer = Math.random();
-if(computerplayer < 0.34) {
-	computerplayer = 'rock';
-} else if (computerplayer <= 0.67) {
-	computerplayer = 'paper';
-} else {
-	computerplayer = 'scissors';
+//separate everything into it's own function.
+//randomly generates a number and sets each to an answer
+results = [];
+function computerPlay() {
+  let num = Math.floor(Math.random() * (4-1) + 1);
+  if(num == 1) {
+    return "rock";
+  } else if(num == 2) {
+    return "paper";
+  } else {
+    return "scissors"
+  }
+}
+//translates the player picks into answers.
+function playerPick(pick) {
+  pick.toLowerCase()
+  if(pick =="rock") {
+      return "rock";
+  } else if(pick =="paper") {
+      return "paper";
+  } else if(pick =="scissors") {
+      return "scissors";
+  } else {
+    alert("only rock, paper or scissors are allowed")
+  }
+}
+//you want to return the results of this call to test this function console.log(playRound(playerSelection, computerSelection));
+//takes the inputs of the computer and human and checks for who wins.
+function playRound(playerSelection, computerSelection) {
+  let winner //0 for computer 1 for player 2 for draw
+  if(playerSelection == computerSelection) {
+      winner = 2;
+  }
+  else if (playerSelection =="rock") {
+    if(computerSelection =="paper") {
+        winner = 0;
+    } else if (computerSelection == "scissors") {
+        winner = 1;
+    }
+  }
+  else if(playerSelection == "scissors") {
+    if(computerSelection == "paper") {
+        winner = 1;
+    } else if(computerSelection == "rock") {
+        winner = 0;
+    }
+  }
+  return winner;
 }
 
-console.log(computerplayer);
-
-
-function playRound(userChoice, computerplayer) {
-	//take two parameters and return a string declaring the winner
-	this.userChoice = userChoice;
-	this.computerplayer = computerplayer;
-	if(userChoice.toUpperCase() === computerplayer.toUpperCase()) {
-		return 'the result is a tie!'
-	}if (userChoice.toUpperCase() === 'ROCK' && computerplayer.toUpperCase() === 'SCISSORS') {
-		return 'The rock wins!';
-	} else {
-		return 'Paper wins!'
-	}
- 
-
-	  if(userChoice.toUpperCase() === 'PAPER' && computerplayer.toUpperCase()=== 'ROCK') {
-		return 'Paper has won!';
-	} else  if(computerSelection.toUpperCase() === 'SCISSORS'){
-		return 'scissors has won'
-	}
-
-	if (userChoice.toUpperCase() === 'SCISSORS' && computerplayer.toUpperCase() === 'ROCK') {
-		return 'Rock wins !';
-	} else if(computerSelection.toUpperCase() ==='PAPER'){
-		return 'scissors wins!';
-		}
-
-	}
-	
-function game(playRound) {
-	i = 0;
-	do {
-		userChoice = prompt('Do you choose rock, paper or scissors?');
-			results.push(playRound(userChoice, computerplayer));
-			console.log(results);
-			i++
-	}
-	while(i < 5);
-}
-game(playRound);
-
-	
+function game() {
+  const computerPick = computerPlay()
+  for(i = 1; i < 6; i++) {
+    let pick = prompt("Round " + i + " rock paper or scissors?")
+      round = playRound(playerPick(pick), computerPick)
+      if(round == 0) {
+        results.push(`You Lose! ${computerPick} beats ${pick}`);
+      } else if(round == 1) {
+        results.push(`You Win! ${pick} beats ${computerPick}`);
+      } else if(round ==2) {
+        results.push(`${pick} draws with ${computerPick}`)
+      }
+    }
+  }
+  game()
+  
+  console.log(results);
